7f6b7287308bb4e6ff14943cd149662fdb293ac2
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-02-24 14:06:19 -0800

    feat(Quotes): add quote details layout and functionality

---
+++ Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/QuoteDetail.java
@@ -0,0 +1,38 @@
+package com.quotes.kaushiknandhan.quotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by kaushik nandhan on 2/24/2017.
+ */
+
+public class QuoteDetail extends AppCompatActivity {
+    private ImageView mImageView;
+    private TextView mQuote;
+    private int mPosition;
+    private DataSource mDataSource;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quote_detail);
+        Intent i = getIntent();
+        mPosition = i.getIntExtra("position", 0);
+
+        mDataSource = new DataSource();
+        mImageView = (ImageView) findViewById(R.id.image);
+        mQuote = (TextView) findViewById(R.id.quote);
+
+        mImageView.setImageResource(mDataSource.getmPhotoHdPool().
+                get(mPosition));
+
+        mQuote.setText(getResources().getString(mDataSource.getmQuotePool()
+                .get(mPosition)));
+
+    }
+}
--- Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/QuoteReaderActivity.java
+++ Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/QuoteReaderActivity.java
@@ -1,14 +1,17 @@
 package com.quotes.kaushiknandhan.quotes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
@@ -24,6 +27,8 @@ public class QuoteReaderActivity extends AppCompatActivity {
         private Context mContext;
         private LayoutInflater mInflator;
         private DataSource mDataSource;
+
+
 
         public QuoteAdapter(Context c) {
             mContext = c;
@@ -68,8 +73,24 @@ public class QuoteReaderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_reader);
+        ListView mListView = (ListView) findViewById(R.id.quotes_list);
+        mListView.setAdapter(new QuoteAdapter(this));
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int position,
+                                    long arg3) {
+                Intent i = new Intent(QuoteReaderActivity.this, QuoteDetail.class);
+                i.putExtra("position", position);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
