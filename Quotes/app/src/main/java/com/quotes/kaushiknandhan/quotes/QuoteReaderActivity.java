8d5a3c77748ed65bd70af094a309ab16f6b7eff6
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-02-24 12:49:23 -0800

    feat(Quotes): add adapter class

--- Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/QuoteReaderActivity.java
+++ Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/QuoteReaderActivity.java
@@ -1,14 +1,76 @@
 package com.quotes.kaushiknandhan.quotes;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class QuoteReaderActivity extends AppCompatActivity {
+
+
+    public class QuoteAdapter extends BaseAdapter {
+
+        private Context mContext;
+        private LayoutInflater mInflator;
+        private DataSource mDataSource;
+
+        public QuoteAdapter(Context c) {
+            mContext = c;
+            mInflator = (LayoutInflater)
+                    mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mDataSource = new DataSource();
+        }
+
+
+        @Override
+        public int getCount() {
+            return mDataSource.getDataSourceLength();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView thumbnail;
+            TextView quote;
+
+            if(convertView == null) {
+                convertView = mInflator.inflate(R.layout.list_item_layout, parent,
+                        false);
+            }
+            thumbnail = (ImageView) convertView.findViewById(R.id.thumb);
+            thumbnail.setImageResource(mDataSource.getmPhotoPool().get(position));
+            quote = (TextView) convertView.findViewById(R.id.text);
+            quote.setText(mDataSource.getmQuotePool().get(position));
+            return convertView;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote_reader);
+
     }
 }
 
