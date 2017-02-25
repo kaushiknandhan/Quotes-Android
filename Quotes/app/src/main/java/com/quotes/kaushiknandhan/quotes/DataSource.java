a452bc2f4fb75aff7c7b7599dea48b926756d234
kaushik nandhan injamuri <kaushik nandhan injamuri>
2017-02-24 18:05:07 -0800

    feat(Quotes): modify Datasiurce file to add additional strings and images

--- Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/DataSource.java
+++ Quotes/app/src/main/java/com/quotes/kaushiknandhan/quotes/DataSource.java
@@ -41,11 +41,16 @@ public class DataSource {
         mPhotoPool.add(R.drawable.steve_3);
         mPhotoPool.add(R.drawable.steve_4);
         mPhotoPool.add(R.drawable.steve_5);
-        mPhotoPool.add(R.drawable.steve_6);
-        mPhotoPool.add(R.drawable.steve_7);
-        mPhotoPool.add(R.drawable.steve_8);
-        mPhotoPool.add(R.drawable.steve_9);
-        mPhotoPool.add(R.drawable.steve_10);
+        mPhotoPool.add(R.drawable.rabindranath_tagore_1);
+        mPhotoPool.add(R.drawable.rabindranath_tagore_2);
+        mPhotoPool.add(R.drawable.rabindranath_tagore_3);
+        mPhotoPool.add(R.drawable.rabindranath_tagore_4);
+        mPhotoPool.add(R.drawable.rabindranath_tagore_5);
+//        mPhotoPool.add(R.drawable.steve_6);
+//        mPhotoPool.add(R.drawable.steve_7);
+//        mPhotoPool.add(R.drawable.steve_8);
+//        mPhotoPool.add(R.drawable.steve_9);
+//        mPhotoPool.add(R.drawable.steve_10);
     }
     private void setupQuotePool() {
         mQuotePool.add(R.string.quote_1);
@@ -53,11 +58,17 @@ public class DataSource {
         mQuotePool.add(R.string.quote_3);
         mQuotePool.add(R.string.quote_4);
         mQuotePool.add(R.string.quote_5);
-        mQuotePool.add(R.string.quote_6);
-        mQuotePool.add(R.string.quote_7);
-        mQuotePool.add(R.string.quote_8);
-        mQuotePool.add(R.string.quote_9);
-        mQuotePool.add(R.string.quote_10);
+        mQuotePool.add(R.string.quote_11);
+        mQuotePool.add(R.string.quote_12);
+        mQuotePool.add(R.string.quote_13);
+        mQuotePool.add(R.string.quote_14);
+        mQuotePool.add(R.string.quote_15);
+
+//        mQuotePool.add(R.string.quote_6);
+//        mQuotePool.add(R.string.quote_7);
+//        mQuotePool.add(R.string.quote_8);
+//        mQuotePool.add(R.string.quote_9);
+//        mQuotePool.add(R.string.quote_10);
 
     }
     private void setupPhotoHDPool() {
@@ -66,11 +77,16 @@ public class DataSource {
         mPhotoHdPool.add(R.drawable.steve_hd_3);
         mPhotoHdPool.add(R.drawable.steve_hd_4);
         mPhotoHdPool.add(R.drawable.steve_hd_5);
-        mPhotoHdPool.add(R.drawable.steve_hd_6);
-        mPhotoHdPool.add(R.drawable.steve_hd_7);
-        mPhotoHdPool.add(R.drawable.steve_hd_8);
-        mPhotoHdPool.add(R.drawable.steve_hd_9);
-        mPhotoHdPool.add(R.drawable.apple_hd);
+        mPhotoHdPool.add(R.drawable.rabindranath_tagore_1_hd);
+        mPhotoHdPool.add(R.drawable.rabindranath_tagore_2_hd);
+        mPhotoHdPool.add(R.drawable.rabindranath_tagore_3_hd);
+        mPhotoHdPool.add(R.drawable.rabindranath_tagore_4_hd);
+        mPhotoHdPool.add(R.drawable.rabindranath_tagore_5_hd);
+//        mPhotoHdPool.add(R.drawable.steve_hd_6);
+//        mPhotoHdPool.add(R.drawable.steve_hd_7);
+//        mPhotoHdPool.add(R.drawable.steve_hd_8);
+//        mPhotoHdPool.add(R.drawable.steve_hd_9);
+//        mPhotoHdPool.add(R.drawable.apple_hd);
     }
 
 
